14081026a10d8482e281f41fcc6923e56155fba8
YeMyintSoeKMD <yms.yemyintsoe.kmd@gmail.com>
2022-12-31 16:44:19 +0630

    yms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    items: [
+      {id: 1, name: 'apple', price: '1000'},
+      {id: 2, name: 'pineapple', price: '2000'},
+      {id: 3, name: 'watermelion', price: '3000'},
+    ]
+  }
+  
+  add = (name, price) => {
+    let id = this.state.items.length + 1
+    this.setState({
+      items: [...this.state.items, {id, name, price}]
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h4>React App</h4>
+        <ul>
+            {
+              this.state.items.map(i => {
+                return (
+                  <Item key={i.id} name={i.name} price={i.price} />
+                )
+              })
+            }
+        </ul>
+        <AddForm add={this.add} />
+      </div>
+    )
+  }
 }
 
-export default App;
+class Item extends React.Component {
+  render() {
+    return (
+      <li> {this.props.name} {this.props.price} </li>
+    )
+  }
+}
+
+class AddForm extends React.Component {
+  nameRef = React.createRef()
+  priceRef = React.createRef()
+  add = () => {
+    let name = this.nameRef.current.value
+    let price = this.priceRef.current.value
+
+    this.props.add(name, price)
+    
+    this.nameRef.current.value = ''
+    this.priceRef.current.value = ''
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.nameRef} /> <br/>
+        <input type="number" ref={this.priceRef} /> <br/>
+        <button onClick={this.add}>Add</button>
+      </div>
+    )
+  }
+}
+
+export default App
